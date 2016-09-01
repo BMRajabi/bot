64730b358b68e6bb73f97b1a692ea386caf03ed4
BMRajabi <behnamrajabi32@yahoo.com>
2016-09-01 15:42:44 +0000

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '248942480:AAHSKjACjp5l-iN9BfyVm68EvsWo9ThCS1o');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
